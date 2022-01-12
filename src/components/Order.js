666f32ef352d0406926a8d3a535178b09c501969
andurebel <andu.trica@gmail.com>
2022-01-12 23:23:33 +0200

    done

--- src/components/Order.js
+++ src/components/Order.js
@@ -31,7 +31,7 @@ export const Order = ({ pizza }) => {
       initial="hidden"
       animate="visible"
     >
-      <h2>Thank you for your order :)</h2>
+      <h2>Thank you for your order!</h2>
       <p>You ordered a {pizza.base} pizza with:</p>
       {pizza.toppings.map((topping) => (
         <motion.div
